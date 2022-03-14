ac88de267b39912da77bd07af2e6e2caee11e9ef
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2022-03-14 20:14:50 +0700

    No Route, adding new controller
    Route is in app.js temporary, need to create a route.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routers')
+const controller = require('./controller')
 
 const app = Express()
 const port = process.env.PORT || 3600
@@ -11,7 +12,10 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-app.use('/', routes)
+app.get('/', controller.index)
+app.get('/scales', controller.getScalesAll)
+app.post(`/scales/add`, controller.insertScale)
+app.get('/scales/:id', controller.getExperimentsByOwner)
 
 app.listen(port, () => {
   console.log(`Substance Measurement API | Listening http://localhost:${port}`)
--- controller.js
+++ controller.js
@@ -5,22 +5,105 @@ exports.index = (req, res) => {
   response.ok("Substance Measurement API works properly!", res)
 }
 
+//Table scales
 exports.getScalesAll = (req, res) => {
-  db.query(`SELECT * FROM scales`, (err, data) => {
+  db.query(`SELECT * FROM scales;`, (err, data) => {
     if (err){
-      console.error(`Problem getting scales data: ${err.stack}`)
+      console.error(`||Error getting all scales data|| \n${err.stack}`)
     }
 
     response.ok(data, res)
   })
 }
 
+exports.getScaleByID = (req, res) => {
+  const id = req.params.id
+  db.query(`SELECT * FROM scales WHERE scales_id = '${id}'`, (err, data) => {
+    if (err){
+      console.error(`||Error getting scale by ID|| \n${err.stack}`)
+    }
+
+    response.ok(data, res)
+  })
+}
+
+exports.insertScale = (req, res) => {
+  const id = req.body.id 
+  const owner = req.body.owner
+  db.query(`INSERT INTO scales (scales_id, scales_owner) VALUES ('${id}', '${owner}')`, (err) => {
+    if (err){
+      console.error(`||Error inserting scale data|| \n${err.stack}`)
+    }
+
+    response.ok('Scale inserted properly!', res)
+  })
+}
+
+exports.deleteScaleByID = (req, res) => {
+  const id = req.params.id
+  db.query(`DELETE FROM scales WHERE scales.scales_id = ${id}`, (err) => {
+    if (err){
+      console.error(`||Error deleting scale by ID|| \n${err.stack}`)
+    }
+
+    response.ok(`Success deleting the scale!`, res)
+  })
+}
+
+//KURANG EDIT
+
+// Table Experiments
 exports.getExperimentsByOwner = (req, res) => {
   const owner = req.params.id
-  db.query(`SELECT * FROM experiments WHERE experiments_owner = ${owner}`, (err, data) => {
+  console.log(owner)
+  db.query(`SELECT * FROM experiments WHERE experiments_id = '${owner}';`, (err, data) => {
     if (err){
-      console.error(`Problem getting experiments data by owner: ${err.stack}`)
+      console.error(`||Error getting experiments data by owner|| \n${err.stack}`)
     }
     response.ok(data, res)
   })
 }
+
+exports.getExperimentsAll = (req, res) => {
+  db.query(`SELECT * FROM experiments;`, (err, data) => {
+    if (err){
+      console.error(`||Error getting all experiments data|| \n${err.stack}`)
+    }
+
+    response.ok(data, res)
+  })
+}
+
+exports.insertExperiment = (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const desc = req.body.desc
+  const owner = req.body.owner
+  db.query(`INSERT INTO experiments (experiments_id, experiments_name, experiments_desc ,experiments_owner) VALUES ('${id}', '${name}', '${desc}', '${owner}');`, (err, data) => {
+    if (err){
+      console.error(`||Error inserting new experiment|| \n${err.stack}`)
+    }
+
+    response.ok('Experiment inserted properly!', res)
+  })
+}
+
+exports.deleteExperimentByID = (req, res) => {
+  const id = req.params.id
+  db.query(`DELETE FROM experiments WHERE experiments.experiments_id = ${id}`, (err, data) => {
+    if (err){
+      console.error(`||Error deleting experiment by ID|| \n${err.stack}`)
+    }
+
+    response.ok('Success deleting the experiment!', res)
+  })
+}
+
+// KURANG UPDATE
+
+
+// Table Records
+exports.getRecordsByExperiment = (req, res) => {
+  const experiment = req.params.experiment
+  db.query(`SELECT * FROM rec`)
+} //INI BELUM SELESE
